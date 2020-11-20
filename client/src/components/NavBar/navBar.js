74e0b146ed4148c9b29f9bebbf4993dc99784d7b
halowe <haleylowe0@gmail.com>
2020-11-19 21:26:44 -0800

    changed file structure. Added public and src folders to client folder. Made basic NavBar in components folder.

---
+++ client/src/components/NavBar/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+    <a className="navbar-brand" href="/">
+        Food Bank Finder
+    </a>
+    </nav>
+    );
+};
+
+export default NavBar;
