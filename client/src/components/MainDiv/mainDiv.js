48722fdbd48ecc45bb7cdb9d3a03404577fa62d0
halowe <haleylowe0@gmail.com>
2020-12-01 19:30:47 -0800

    commit changes in mainDv and citySearch

--- client/src/components/MainDiv/mainDiv.js
+++ client/src/components/MainDiv/mainDiv.js
@@ -1,10 +1,12 @@
+import React from "react";
 import NavBar from "../NavBar/NavBar";
 import "../NavBar/NavBar.css";
 import FoodBankLocations from "../FoodBankLocations/foodBankLocations";
 import GoogleMap from "../GoogleMap/googleMap";
 import HeaderComps from "../HeaderComps/HeaderComps";
-import Button from "react-bootstrap/Button";
-import CitySearch from "../CitySearch/CitySearch";
+// import Button from "react-bootstrap/Button";
+// import CitySearch from "../CitySearch/CitySearch";
+import CardContainer from "../Card/CardContainer";
 //import Aside from '../Aside/aside';
 
 function MainDiv() {
@@ -12,11 +14,12 @@ function MainDiv() {
     <div className="m-4 mainDiv mx-auto">
       <NavBar />
       <HeaderComps />
-        <div>
+      {/* <div>
           <CitySearch />
           <Button>Search Location</Button>
-        </div>
-      <FoodBankLocations/>
+        </div> */}
+      <CardContainer />
+      <FoodBankLocations />
       <GoogleMap />
     </div>
   );
